eb0d879e7e31417cf6e2f839d06d25e8075b3f83
Gabriel <santos.gabriel@aluno.ufca.edu.br>
2026-05-13 14:56:15 -0300

    initial commit

---
+++ src/main/java/io/github/gabriel_so_santos/jcinema/JCinemaApplication.java
@@ -0,0 +1,11 @@
+package io.github.gabriel_so_santos.jcinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JCinemaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JCinemaApplication.class, args);
+	}
+}
---
+++ src/test/java/io/github/gabriel_so_santos/jcinema/JCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gabriel_so_santos.jcinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JCinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
