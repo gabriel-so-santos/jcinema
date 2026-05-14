2aeccc8e706543b980e933779dca76bf5c10ad54
Gabriel <santos.gabriel@aluno.ufca.edu.br>
2026-05-14 13:16:26 -0300

    feat(repository): create `CategoryRepository`

---
+++ src/main/java/io/github/gabriel_so_santos/jcinema/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package io.github.gabriel_so_santos.jcinema.repository;
+
+import io.github.gabriel_so_santos.jcinema.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+
+}
