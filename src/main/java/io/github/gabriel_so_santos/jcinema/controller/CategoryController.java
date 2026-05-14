c036a894d0fbe959bdebc4e664d525fea829138f
Gabriel <santos.gabriel@aluno.ufca.edu.br>
2026-05-14 17:30:13 -0300

    feat(controller): add CRUD endpoints to `CategoryController`

--- src/main/java/io/github/gabriel_so_santos/jcinema/controller/CategoryController.java
+++ src/main/java/io/github/gabriel_so_santos/jcinema/controller/CategoryController.java
@@ -3,11 +3,10 @@ package io.github.gabriel_so_santos.jcinema.controller;
 import io.github.gabriel_so_santos.jcinema.entity.Category;
 import io.github.gabriel_so_santos.jcinema.service.CategoryService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/jcinema/category")
@@ -18,5 +17,21 @@ public class CategoryController {
     @GetMapping
     public List<Category> getAll(){
         return categoryService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Category getById(@PathVariable Long id){
+        Optional<Category> category = categoryService.findById(id);
+        return category.orElse(null);
+    }
+
+    @PostMapping
+    public Category save(@RequestBody Category category){
+        return categoryService.save(category);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Long id){
+        categoryService.deleteById(id);
     }
 }
