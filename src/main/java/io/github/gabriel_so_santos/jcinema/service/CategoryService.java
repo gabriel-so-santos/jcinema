faa867a79136620c8fd3400f1699f9a8b9f7186e
Gabriel <santos.gabriel@aluno.ufca.edu.br>
2026-05-14 17:28:40 -0300

    feat(service): implement basic CRUD logic for `Category`

--- src/main/java/io/github/gabriel_so_santos/jcinema/service/CategoryService.java
+++ src/main/java/io/github/gabriel_so_santos/jcinema/service/CategoryService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,5 +15,17 @@ public class CategoryService {
 
     public List<Category> findAll(){
         return categoryRepository.findAll();
+    }
+
+    public Optional<Category> findById(Long id){
+        return categoryRepository.findById(id);
+    }
+
+    public Category save(Category category){
+        return categoryRepository.save(category);
+    }
+
+    public void deleteById(Long id){
+        categoryRepository.deleteById(id);
     }
 }
