ca461b488f9395df891033df710e4154a8c5e6e9
Gabriel <santos.gabriel@aluno.ufca.edu.br>
2026-05-16 16:11:39 -0300

    feat(entity): create `StreamingPlatform` entity

---
+++ src/main/java/io/github/gabriel_so_santos/jcinema/entity/StreamingPlatform.java
@@ -0,0 +1,21 @@
+package io.github.gabriel_so_santos.jcinema.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name="streaming_platform")
+public class StreamingPlatform {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false, unique = true)
+    private String name;
+}
+
