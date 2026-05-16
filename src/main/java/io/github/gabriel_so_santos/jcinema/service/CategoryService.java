09568ba46000ebdb535b10b50e73e957af62577e
Gabriel <santos.gabriel@aluno.ufca.edu.br>
2026-05-15 23:29:21 -0300

    style: adjust spaces between braces

--- src/main/java/io/github/gabriel_so_santos/jcinema/service/CategoryService.java
+++ src/main/java/io/github/gabriel_so_santos/jcinema/service/CategoryService.java
@@ -13,19 +13,19 @@ import java.util.Optional;
 public class CategoryService {
     private final CategoryRepository categoryRepository;
 
-    public List<Category> findAll(){
+    public List<Category> findAll() {
         return categoryRepository.findAll();
     }
 
-    public Optional<Category> findById(Long id){
+    public Optional<Category> findById(Long id) {
         return categoryRepository.findById(id);
     }
 
-    public Category save(Category category){
+    public Category save(Category category) {
         return categoryRepository.save(category);
     }
 
-    public void deleteById(Long id){
+    public void deleteById(Long id) {
         categoryRepository.deleteById(id);
     }
 }
