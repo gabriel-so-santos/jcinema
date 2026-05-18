4b4f5799170ffdd05c8dc1a8a912737245c039a6
Gabriel <santos.gabriel@aluno.ufca.edu.br>
2026-05-18 15:42:05 -0300

    feat(db): create table `movie`

---
+++ src/main/resources/db/migration/V3__create_table_movie.sql
@@ -0,0 +1,9 @@
+CREATE TABLE movie(
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    description TEXT,
+    release_date DATE,
+    rating NUMERIC,
+    created_at TIMESTAMPTZ NOT NULL,
+    updated_at TIMESTAMPTZ NOT NULL,
+);
