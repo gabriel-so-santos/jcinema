11f8f0e50a742251101fe357c832e45ae05852fe
Gabriel <santos.gabriel@aluno.ufca.edu.br>
2026-05-18 14:52:42 -0300

    feat(entity): create `Movie`

---
+++ src/main/java/io/github/gabriel_so_santos/jcinema/entity/Movie.java
@@ -0,0 +1,43 @@
+package io.github.gabriel_so_santos.jcinema.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "movie")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false, unique = true)
+    private String title;
+
+    @Column
+    private String description;
+
+    @Column
+    private double rating;
+
+    @Column(name = "release_date")
+    private LocalDate releaseDate;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private Instant createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    @UpdateTimestamp
+    private Instant updatedAt;
+}
+
