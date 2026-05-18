dbea1075057161e136fe7f1eef222a411bf93fec
Gabriel <santos.gabriel@aluno.ufca.edu.br>
2026-05-18 14:54:35 -0300

    feat(service): create `MovieService`

---
+++ src/main/java/io/github/gabriel_so_santos/jcinema/service/MovieService.java
@@ -0,0 +1,31 @@
+package io.github.gabriel_so_santos.jcinema.service;
+
+import io.github.gabriel_so_santos.jcinema.entity.Movie;
+import io.github.gabriel_so_santos.jcinema.repository.MovieRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MovieService {
+    private MovieRepository movieRepository;
+
+    public List<Movie> findAll() {
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> findById(Long id) {
+        return movieRepository.findById(id);
+    }
+
+    public Movie save(Movie movie) {
+        return movieRepository.save(movie);
+    }
+
+    public void deleteById(Long id) {
+        movieRepository.deleteById(id);
+    }
+}
